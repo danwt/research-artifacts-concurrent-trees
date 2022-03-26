29008bee9c2605cfd1e1ea7a5b53772a9b0fa08b
Daniel <danwtisdall@gmail.com>
2022-03-26 17:40:18 +0000

    deletes third_party

--- third_party/brown/implementations/cpp/3path_htm/hybridnorec/hytm1/testing.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-g++ hytm1.c testing.cpp -lpthread -o testing.out
-if [ $? -eq 0 ]; then
-    ./testing.out
-else
-    echo "Error during compilation!"
-fi
--- third_party/brown/implementations/cpp/range_queries/common/test_htm_support.cpp
+++
@@ -1,6 +0,0 @@
-#include "rtm.h"
-int main(int argc, char** argv) {
-    XBEGIN();
-    XEND();
-    return 0;
-}
